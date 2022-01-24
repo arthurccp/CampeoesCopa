31925fd85cfc5c9dca038458e5ba93e275ea2c2f
Arthur Silva <arthutquirinos@gmail.com>
2022-01-24 14:30:43 -0300

    commit de insercao

---
+++ CampeoesDaCopa/Controller/WinnersTableViewController.swift
@@ -0,0 +1,107 @@
+
+
+import UIKit
+
+class WinnersTableViewController: UITableViewController {
+    
+    var worldCups: [WorldCup] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadWordCups()
+    }
+    
+    func loadWordCups(){
+        let fileUrl = Bundle.main.url(forResource: "winners.json", withExtension: nil)!
+        let jsonData = try! Data(contentsOf: fileUrl)
+        do{
+            worldCups = try JSONDecoder().decode([WorldCup].self, from: jsonData)
+        }catch{
+            print(error)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let vc = segue.destination as! WorldCupViewController
+        let worldCup = worldCups[tableView.indexPathForSelectedRow!.row]
+        vc.worldCup = worldCup
+    }
+    
+    
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 1
+//    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return worldCups.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! WorldCupTableViewCell
+
+        let worldCup = worldCups[indexPath.row]
+        cell.prepare(with: worldCup)
+        
+        
+        
+        
+//        cell.textLabel?.text = "Copa \(worldCup.year) - \(worldCup.country)"      preenche as celular respectivas
+//        cell.detailTextLabel?.text = "\(worldCup.winner) - \(worldCup.vice)"
+//        cell.imageView?.image = UIImage(named: "\(worldCup.winner).png")
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Lista Paises que disputaram a copa do Mundo
+
+O app consome um arquivo json e lista em uma tableView dinâmica todos os paises que disputaram a copa do mundo acompanhado do resultado final do jogo, podendo observar também os jogos anteriores como as oitavas, semifinal que lavaram o Pais ao referido lugar.  
